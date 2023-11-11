c7e1b4ae3eb3f70c4684500b2c5308ed13040f85
Khalid Graine <khalidgraine1@gmail.com>
2023-11-11 14:48:49 +0100

    Making a POST Request

--- src/Create.jsx
+++ src/Create.jsx
@@ -2,26 +2,50 @@ import { useState } from "react";
 
 const CreateNewBook = () => {
   const [title, setTitle] = useState("");
-  const [author,setAuthor] = useState("");
+  const [author, setAuthor] = useState("");
+  const book = { title, author };
+
+  const [loading,setIsLoading] = useState(false);
+
   const hundleSubmit = (e) => {
     e.preventDefault();
-    const book = {title,author};
-
-  }
+     setIsLoading(true)
+    fetch("http://localhost:8000/books/", {
+      method: "post",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(book),
+    }).then((res) => {
+        if(res.ok) {
+          setIsLoading(false)
+        }
+     
+    });
+  };
   return (
     <div className="bg-gray-400 p-4">
       <form onSubmit={(e) => hundleSubmit(e)} className="flex flex-col gap-2">
         <label>title</label>
-        <input type="text" 
-        onChange={(e) => setTitle(e.target.value)} 
-        value={title} />
+        <input
+          type="text"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
+        />
 
-        <label >
-        author
-        </label>
-        <input type="text" onChange={(e) => setAuthor(e.target.value)} value={author} />
+        <label>author</label>
+        <input
+          type="text"
+          onChange={(e) => setAuthor(e.target.value)}
+          value={author}
+        />
 
-        <button type="submit"  className="bg-blue-500 p-2 rounded-sm text-white">Submit</button>
+        <button type="submit" className="bg-blue-500 p-2 rounded-sm text-white">
+          Submit
+        </button>
+      {loading &&  <p className="bg-green-300"> loading ...</p>}
+
       </form>
     </div>
   );
