21d8c069e1d81a79ad5b125caece900ef0596d71
Khalid Graine <khalidgraine1@gmail.com>
2023-11-11 14:31:45 +0100

    Submit Events

--- src/Create.jsx
+++ src/Create.jsx
@@ -3,10 +3,14 @@ import { useState } from "react";
 const CreateNewBook = () => {
   const [title, setTitle] = useState("");
   const [author,setAuthor] = useState("");
+  const hundleSubmit = (e) => {
+    e.preventDefault();
+    const book = {title,author};
 
+  }
   return (
     <div className="bg-gray-400 p-4">
-      <form>
+      <form onSubmit={(e) => hundleSubmit(e)} className="flex flex-col gap-2">
         <label>title</label>
         <input type="text" 
         onChange={(e) => setTitle(e.target.value)} 
@@ -17,9 +21,8 @@ const CreateNewBook = () => {
         </label>
         <input type="text" onChange={(e) => setAuthor(e.target.value)} value={author} />
 
-       
+        <button type="submit"  className="bg-blue-500 p-2 rounded-sm text-white">Submit</button>
       </form>
-      <p>{author}</p>
     </div>
   );
 };
