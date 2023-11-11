1e02bcdaf2813d0391a63e645fdea8a9d4cad72b
Khalid Graine <khalidgraine1@gmail.com>
2023-11-11 14:00:17 +0100

    Reusing Custom Hooks

--- src/BookDetails.jsx
+++ src/BookDetails.jsx
@@ -1,12 +1,23 @@
-import {useParams} from "react-router-dom"
+import { useParams } from "react-router-dom";
+import useFetch from "./usefetch";
 
 const BookDetails = () => {
-    const { id } = useParams()
-    return ( 
-        <div>
-            books details - {id}
-        </div>
-     );
-}
- 
+  const { id } = useParams();
+  const { data: book, error, isLoading } = useFetch(
+    "http://localhost:8000/books/" + id
+  );
+
+  return (
+    <div>
+      {error && <div className="text-red-500">{error}</div>}
+      {isLoading && <p>loading ... </p>}
+      {book && 
+      <div>
+        {book.title}
+      </div>
+      }
+    </div>
+  );
+};
+
 export default BookDetails;
